f659f9f47ef884c9b0bd1fcfb2c80ac23cdb31ec
ertheis <ertheis@Erics-MacBook-Pro-7.local>
2014-06-26 11:50:21 -0700

    Initial Commit

---
+++ PubNubGettingStarted/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PubNubGettingStarted
+//
+//  Created by Eric Theis on 6/26/14.
+//  Copyright (c) 2014 PubNub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
