9415c3782a904172534f61bc0d52aa7fe54d6080
ertheis <ertheis@Erics-MacBook-Pro-7.local>
2014-06-26 12:38:37 -0700

    Added Getting Started Reop

---
+++ PubNubGettingStarted/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  bridgingHeader.h
+//  PubNubGettingStarted
+//
+//  Created by Eric Theis on 6/26/14.
+//  Copyright (c) 2014 PubNub. All rights reserved.
+//
+
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import "PNImports.h" // Add this as the LAST import statement
+#endif
